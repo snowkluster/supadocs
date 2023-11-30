b9c7b138072b45d5cbabcd21264a6ef76845b1b8
Rohan Kaushik <rohankaushik1@outlook.com>
2023-11-30 23:27:51 +0530

    working delta type of non varient quill change event

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Editor from "./components/Editor";
 import "./styles/Editor.css"
+import { BrowserRouter, Routes , Route, Navigate } from "react-router-dom";
 import { supabase } from "./config/supabaseClient";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function App() {
   console.log(supabase)
   return (
-    <>
-      <Editor />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate replace to={`/docs/${uuidv4()}`} />} />
+        <Route path="/docs/:id" element={<Editor />} /> 
+      </Routes>
+    </BrowserRouter>
   )
 }
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,46 +1,92 @@
 import { Box } from "@mui/material";
-import { useEffect } from "react";
-import styled from "@emotion/styled"
+import { useEffect, useState } from "react";
+import styled from "@emotion/styled";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
-
+import { Socket, io } from "socket.io-client";
+import Delta from "quill-delta";
 
 const Component = styled.div`
-    background: #F5F5F5;
-
-`
+  background: #f5f5f5;
+`;
 
 export default function Editor() {
+  const [socket, setSocket] = useState<Socket>();
+  const [quill, setQuill] = useState<Quill>();
+
   useEffect(() => {
     const toolbarOptions = [
-        ["bold", "italic", "underline", "strike"], // toggled buttons
-        ["blockquote", "code-block"],
-    
-        [{ header: 1 }, { header: 2 }], // custom button values
-        [{ list: "ordered" }, { list: "bullet" }],
-        [{ script: "sub" }, { script: "super" }], // superscript/subscript
-        [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
-        [{ direction: "rtl" }], // text direction
-    
-        [{ size: ["small", false, "large", "huge"] }], // custom dropdown
-        [{ header: [1, 2, 3, 4, 5, 6, false] }],
-    
-        [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-        [{ font: [] }],
-        [{ align: [] }],
-    
-        ["clean"], // remove formatting button
-      ];
-    new Quill("#container", {
+      ["bold", "italic", "underline", "strike"], // toggled buttons
+      ["blockquote", "code-block"],
+
+      [{ header: 1 }, { header: 2 }], // custom button values
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ script: "sub" }, { script: "super" }], // superscript/subscript
+      [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+      [{ direction: "rtl" }], // text direction
+
+      [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+      [{ font: [] }],
+      [{ align: [] }],
+
+      ["clean"], // remove formatting button
+    ];
+    const quillServer = new Quill("#container", {
       modules: {
         toolbar: toolbarOptions,
       },
       theme: "snow",
     });
+    setQuill(quillServer);
   }, []);
+  useEffect(() => {
+    const socketServer = io("http://localhost:3000");
+    setSocket(socketServer);
+
+    return () => {
+      socketServer.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if ((socket as Socket) === null || (quill as Quill) === null) return;
+
+    const handleChange = (
+      delta: unknown,
+      _oldData: unknown,
+      source: string
+    ) => {
+      if (source !== "user") return;
+      (socket as Socket).emit("send-changes", delta);
+    };
+
+    (quill as Quill) && (quill as Quill).on("text-change", handleChange);
+
+    return () => {
+      (quill as Quill) && (quill as Quill).off("text-change", handleChange);
+    };
+  }, [quill, socket]);
+
+  useEffect(() => {
+    if ((socket as Socket) === null || (quill as Quill) === null) return;
+    type Delta = InstanceType<typeof Delta> 
+    const handleChange = (delta: Delta) => {
+      (quill as Quill).updateContents(delta);
+    };
+
+    socket && socket.on("receive-changes", handleChange);
+
+    return () => {
+      socket && socket.off("receive-changes", handleChange);
+    };
+  }, [quill, socket]);
+
   return (
     <Component>
-      <Box id="container" className='container'></Box>
+      <Box id="container" className="container"></Box>
     </Component>
   );
 }
